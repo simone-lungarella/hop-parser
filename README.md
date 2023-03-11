4289a799ca1b476204ff923417fe07f49fd2ca7e
Simone Lungarella <simonelungarella@gmail.com>
2023-03-11 15:49:53 +0100

    Fixed alghoritm to avoid swith face duplicated commands

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 <img src="https://img.shields.io/badge/Powershell-2CA5E0?style=for-the-badge&logo=powershell&logoColor=white" />
 
 
-This is a script that parses files having extension <b>.hop</b> to search for duplicated lines that make a <i>CNC machine</i> stop, select a tool, and restart when selecting the tool that is already being used. This behavior is caused by the <i>WZ*</i> command, which is used to select a tool in more modern CNC machines while not causing any issues with older ones.
+This is a script that parses files having extension <b>.hop</b> to search for duplicated lines that make a <i>CNC machine</i> stop, execute an operation, and restart when selecting the tool that is already being used or a face that is already set. This behavior is caused by the <i>WZ*</i> and <i>EBENE*</i> commands when these commands are duplicated in more modern CNC machines while not causing any issues with older ones.
 
-This simple program iterates over a root folder required as a parameter and recursively searches for files with extension <b>.hop</b>. Then, it parses each file, and for each line starting with <i>WZ</i>: if it's equal to the previously assigned tool, the line will be commented out by adding a semicolon at the start.
+This simple program iterates over a root folder required as a parameter and recursively searches for files with extension <b>.hop</b>. Then, it parses each file, and for each line starting with <i>WZ</i>: if it's equal to the previously assigned tool, the line will be commented out by adding a semicolon at the start. Does the same for commands starting with <i>EBENE</i> and the previously assigned face.
 
 ## Usage in Powershell
 
--- hop-parser.ps1
+++ hop-parser.ps1
@@ -1,36 +1,55 @@
-$source=Read-Host "Please enter your path source folder with HOP files"
+$source = Read-Host "Please enter your path source folder with HOP files"
 Set-Location $source
 
 Get-ChildItem -Recurse |
-    Where-Object {!($_.PSIsContainer)} |
-    ForEach-Object {
-        $prevLine = ""
-        $label = ""
+Where-Object { !($_.PSIsContainer) } |
+ForEach-Object {
+    $lastTool = ""
+    $lastFace = ""
 
-        if ($_.Extension -ne ".hop") {
-            Write-Output "Skipping file: $($_.FullName)"
-            return
+    if ($_.Extension -ne ".hop") {
+        Write-Output "Skipping file: $($_.FullName)"
+        return
+    }
+
+    Write-Output "Processing file: $($_.FullName)"
+    $isChanged = $false
+        
+    $content = Get-Content $_.FullName | Where-Object { $_ -notmatch "^;" } |
+    ForEach-Object {
+        $line = $_
+
+        if ($line.StartsWith("WZ")) {
+            # Cerca comando selezione utensile
+            if ($lastTool -ne "" -and $line -eq $lastTool) {
+                # Se l'utensile è già equipaggiato, commenta riga
+                $isChanged = $false
+                $line = "; $line"
+            }
+            else {
+                # Altrimenti, aggiorna lo strumento corrente
+                $isChanged = $true
+                $lastTool = $line
+            }
+        }
+        elseif ($line.StartsWith("EBENE")) {
+            # Se l'utensile non è cambiato, cerca comando selezione faccia
+            if (!$isChanged -and $lastFace -ne "" -and $line -eq $lastFace) {
+                # Se la faccia è già selezionata, commenta riga
+                $line = "; $line"
+            }
+            else {
+                # Altrimenti, aggiorna la faccia corrente
+                $lastFace = $line
+            }
         }
 
-        Write-Output "Processing file: $($_.FullName)"
-        $content = Get-Content $_.FullName |
-            ForEach-Object {
-                $line = $_
-
-                if ($line.StartsWith("WZ")) {
-                    if ($label -ne "" -and $line -eq $label) {
-                        $line = ";$line"
-                    } else {
-                        $label = $line
-                    }
-                }
-
-                $prevLine = $line
-                $line
-            }
-
-        $content | Set-Content $_.FullName
+        $line
     }
+
+    $content | Set-Content $_.FullName
+} 
+
 
 Write-Output "Done. Press any key to exit..."
 $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
